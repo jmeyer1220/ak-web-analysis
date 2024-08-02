c98b6541bcc6d4fe19e789375e8557efce72215a
downtothemeyer <jmeyer1220@gmail.com>
2024-08-02 09:18:16 -0500

    Update crawl.js

--- pages/api/crawl.js
+++ pages/api/crawl.js
@@ -1,16 +1,68 @@
 import axios from "axios";
 import cheerio from "cheerio";
 import url from "url";
+import { parseString } from "xml2js";
+
+async function crawlPage(targetUrl, contentTypes) {
+  const { data } = await axios.get(targetUrl);
+  const $ = cheerio.load(data);
+  const links = $("a");
+
+  links.each((index, element) => {
+    const href = $(element).attr("href");
+    if (href) {
+      const parsedUrl = url.parse(href);
+      const path = parsedUrl.pathname;
+      if (path) {
+        if (path.includes("/events/")) {
+          contentTypes.events++;
+        } else if (path.includes("/sermons/")) {
+          contentTypes.sermons++;
+        } else if (path.includes("/news/")) {
+          contentTypes.news++;
+        } else if (path.includes("/blog/")) {
+          contentTypes.blog++;
+        } else if (path.endsWith(".html") || path.endsWith("/") || path === "") {
+          contentTypes.pages++;
+        } else {
+          contentTypes.other++;
+        }
+      }
+    }
+  });
+
+  return contentTypes;
+}
+
+async function crawlSitemap(sitemapUrl, contentTypes) {
+  const { data } = await axios.get(sitemapUrl);
+  return new Promise((resolve, reject) => {
+    parseString(data, async (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        const urls = result.sitemapindex ? 
+          result.sitemapindex.sitemap.map(sitemap => sitemap.loc[0]) :
+          result.urlset.url.map(url => url.loc[0]);
+
+        for (const url of urls) {
+          if (url.endsWith('.xml')) {
+            await crawlSitemap(url, contentTypes);
+          } else {
+            contentTypes.pages++;
+          }
+        }
+        resolve(contentTypes);
+      }
+    });
+  });
+}
 
 export default async function handler(req, res) {
   const { url: targetUrl } = req.query;
-  
+
   try {
-    const { data } = await axios.get(targetUrl);
-    const $ = cheerio.load(data);
-    const links = $("a");
-    
-    const contentTypes = {
+    let contentTypes = {
       pages: 0,
       events: 0,
       sermons: 0,
@@ -19,43 +71,23 @@ export default async function handler(req, res) {
       other: 0
     };
 
-    links.each((index, element) => {
-      const href = $(element).attr("href");
-      if (href) {
-        const parsedUrl = url.parse(href);
-        const path = parsedUrl.pathname;
-
-        if (path) {
-          if (path.includes("/events/")) {
-            contentTypes.events++;
-          } else if (path.includes("/sermons/")) {
-            contentTypes.sermons++;
-          } else if (path.includes("/news/")) {
-            contentTypes.news++;
-          } else if (path.includes("/blog/")) {
-            contentTypes.blog++;
-          } else if (path.endsWith(".html") || path.endsWith("/") || path === "") {
-            contentTypes.pages++;
-          } else {
-            contentTypes.other++;
-          }
-        }
-      }
-    });
+    if (targetUrl.endsWith('.xml')) {
+      contentTypes = await crawlSitemap(targetUrl, contentTypes);
+    } else {
+      contentTypes = await crawlPage(targetUrl, contentTypes);
+    }
 
     const totalCount = Object.values(contentTypes).reduce((a, b) => a + b, 0);
 
     res.status(200).json({
       pageCount: totalCount,
       contentTypes: contentTypes,
-      contentTypeBreakdown: {
-        events: (contentTypes.events / totalCount * 100).toFixed(2) + '%',
-        sermons: (contentTypes.sermons / totalCount * 100).toFixed(2) + '%',
-        news: (contentTypes.news / totalCount * 100).toFixed(2) + '%',
-        blog: (contentTypes.blog / totalCount * 100).toFixed(2) + '%',
-        pages: (contentTypes.pages / totalCount * 100).toFixed(2) + '%',
-        other: (contentTypes.other / totalCount * 100).toFixed(2) + '%'
-      }
+      contentTypeBreakdown: Object.fromEntries(
+        Object.entries(contentTypes).map(([key, value]) => [
+          key,
+          `${((value / totalCount) * 100).toFixed(2)}%`
+        ])
+      )
     });
   } catch (error) {
     console.error("Error crawling the site:", error.message, error.stack);
